a8e22181cbed1f4838dde20f77f732ec89e5a917
Samuel Garcia Moreira <samuelgarciamoreira1985@gmail.com>
2025-07-07 19:19:58 -0300

    3_Avancando - imagens em assets

--- 3_AVANCANDO_EM_REACT/avancando/src/App.jsx
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
 
+// 2 - imagem em assets - importando
+import night from "./assets/night.jpg";
+
 function App() {
   return (
     <div className='App'>
       <h1>Avançando em React</h1>
       {/* 1 - imagem em public */}
       <img src="/img.jpg" alt="Alguma imagem" />
+      {/* 1 - imagem em assets */}
+      <img src={night} alt="Outra imagem" />
     </div>
   )
 }
