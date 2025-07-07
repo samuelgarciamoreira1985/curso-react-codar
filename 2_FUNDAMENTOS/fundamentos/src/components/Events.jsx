df5a137851a4606e2cc9505702bdde3afbac01f7
Samuel Garcia Moreira <samuelgarciamoreira1985@gmail.com>
2025-07-07 19:09:47 -0300

    3_Avancando_em_React - imagens

--- 2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
+++ 2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
@@ -7,11 +7,33 @@ import React from 'react'
     soma = n1 + n2
     alert(soma)
 }*/
+const CalculoMedia = (n1,n2) => {
+        n1 = 1
+        n2 = 4
+        var res = n1 + n2
+        if (res >= 5){
+            return alert("passou")
+        }else {
+            return alert("Não passou")
+        }
+    }
 
 function Events() {
     const handleClick = (e) => {
         alert("Execultou")
     }
+
+    // 8 - Função de renderização
+    const renderSomething = (x) => {
+        if (x) {
+            return <h1>Renderizando isso!</h1>
+        }else{
+            return <h1>Renderizando outra coisa!</h1>
+        }
+    }
+
+    // return 10 > 2 && <p>Carregando...</p>
+
   return (
     <div>
         <div>
@@ -19,7 +41,11 @@ function Events() {
         </div>
         <div> {/*7 - Evento com função*/}
             <button onClick={handleClick}>Clique aqui - com função</button>
+            <div><button onClick={CalculoMedia}>Calcular</button></div>
         </div>
+        {renderSomething(true)}
+        {renderSomething(false)}
+        
     </div>
   )
 }
