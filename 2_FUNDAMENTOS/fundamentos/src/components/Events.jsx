e4087fc7a6fa944c8dfa0b879c4dd90419e9fa80
Samuel Garcia Moreira <samuelgarciamoreira1985@gmail.com>
2025-07-07 11:04:05 -0300

    2Fundamentos - eventos com funcoes

--- 2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
+++ 2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 
+/*const Calculo = () => {
+    var n1,n2,soma
+    n1 = 2
+    n2 = 54
+    soma = n1 + n2
+    alert(soma)
+}*/
+
 function Events() {
+    const handleClick = (e) => {
+        alert("Execultou")
+    }
   return (
     <div>
         <div>
             <button onClick={() => alert("Testando um evento")}>Clique aqui</button>
+        </div>
+        <div> {/*7 - Evento com função*/}
+            <button onClick={handleClick}>Clique aqui - com função</button>
         </div>
     </div>
   )
