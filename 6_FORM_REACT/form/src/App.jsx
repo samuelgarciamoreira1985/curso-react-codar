2d6fe08c5f6268e753395b0fb1abde6a8b5983b6
Samuel Garcia Moreira <samuelgarciamoreira1985@gmail.com>
2025-07-16 11:15:21 -0300

    6_FORMS EM REACT - gerenciar dados com input

---
+++ 6_FORM_REACT/form/src/App.jsx
@@ -0,0 +1,16 @@
+import MyForm from './components/MyForm'
+import './App.css'
+
+function App() {
+
+  return (
+
+    <div className='App'>
+      <h1>Form em React</h1>
+      <MyForm/>
+    </div>
+
+  )
+}
+
+export default App
---
+++ 6_FORM_REACT/form/src/components/MyForm.jsx
@@ -0,0 +1,39 @@
+import "./MyForm.css"
+
+import { useState } from 'react'
+
+const MyForm = () => {
+
+    // 3 - gerênciamento de dados
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+
+    const handleName = (e) => {
+        setName(e.target.value)
+    }
+
+    console.log(name,email)
+
+  return (
+
+    <div>
+        {/* 1 - criação de form */}
+        <form>
+            <div>
+                <label htmlFor="name">Nome:</label>
+                <input type="text" name="name" placeholder="Digite seu nome" onChange={handleName}/>
+            </div>
+            {/* 2 - label envolvendo input */}
+            <label>
+                <span>E-mail:</span>
+                <input type="text" name="email" placeholder="Digite o seu e-mail" 
+                // 4 - simplificando manutenção
+                onChange={(e) => setEmail(e.target.value)}/>
+            </label>
+            <input type="submit" value="Enviar"/>
+        </form>
+    </div>
+  )
+}
+
+export default MyForm
