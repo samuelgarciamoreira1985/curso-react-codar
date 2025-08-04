c1c5d3a350115da147051031142b4919197a3cbf
Samuel Garcia Moreira <samuelgarciamoreira1985@gmail.com>
2025-08-04 11:50:48 -0300

    11_QUIZ - atualizado 04/08

--- 11_QUIZ/quiz/src/App.jsx
+++ 11_QUIZ/quiz/src/App.jsx
@@ -1,14 +1,27 @@
 import './App.css'
+import { useContext, useEffect } from 'react'
+import { QuizContext } from './context/quiz'
 
 import Welcome from './components/Welcome'
+import Question from './components/Question'
+import GameOver from './components/GameOver'
 
 function App() {
+
+  const [quizState, dispatch] = useContext(QuizContext)
+
+  useEffect(() => {
+    // Embaralhar as perguntas
+    dispatch({type: "REORDER_QUESTIONS"})
+  },[])
 
   return (
 
     <div className='App'>
       <h1>Quiz de Programação</h1>
-      <Welcome/>
+      {quizState.gameStage === "Start" &&   <Welcome/>}
+      {quizState.gameStage === "Playing" &&   <Question/>}
+      {quizState.gameStage === "End" &&   <GameOver/>}
     </div>
 
   )
