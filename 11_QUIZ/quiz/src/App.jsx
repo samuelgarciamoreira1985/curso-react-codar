f0b269d4928ff4ca9a4e412403ae37a6768d93e2
Samuel Garcia Moreira <samuelgarciamoreira1985@gmail.com>
2025-08-06 11:48:22 -0300

    11_QUIZ - atualizado

--- 11_QUIZ/quiz/src/App.jsx
+++ 11_QUIZ/quiz/src/App.jsx
@@ -5,6 +5,7 @@ import { QuizContext } from './context/quiz'
 import Welcome from './components/Welcome'
 import Question from './components/Question'
 import GameOver from './components/GameOver'
+import PickCategory from './components/PickCategory'
 
 function App() {
 
@@ -20,6 +21,7 @@ function App() {
     <div className='App'>
       <h1>Quiz de Programação</h1>
       {quizState.gameStage === "Start" &&   <Welcome/>}
+      {quizState.gameStage === "Category" &&   <PickCategory/>}
       {quizState.gameStage === "Playing" &&   <Question/>}
       {quizState.gameStage === "End" &&   <GameOver/>}
     </div>
--- 11_QUIZ/quiz/src/components/Question.jsx
+++ 11_QUIZ/quiz/src/components/Question.jsx
@@ -1,6 +1,8 @@
 import { useContext } from 'react'
 import { QuizContext } from '../context/quiz'
 
+import Option from './Option'
+
 import "./Question.css"
 
 const Question = () => {
@@ -8,15 +10,30 @@ const Question = () => {
     const [quizState, dispatch] = useContext(QuizContext)
     const currentQuestion = quizState.questions[quizState.currentQuestion]
 
+    const onSelectOption = (option) => {
+      dispatch({
+        type: "CHECK_ANSWER",
+        payload: { answer: currentQuestion.answer, option }
+      })
+    }
+
   return (
 
     <div id='question'>
       <p>Pergunta {quizState.currentQuestion + 1} a {quizState.questions.length}</p>
       <h2>{currentQuestion.question}</h2>
       <div id='options-container'>
-        <p>Opções</p>
+        {currentQuestion.options.map((option) => (
+          <Option option={option} 
+          key={option} 
+          answer={currentQuestion.answer}
+          selectOption={() => onSelectOption(option)}
+          />
+        ))}
       </div>
-        <button onClick={() => dispatch({type: "CHANGE_QUESTION"})}>Continuar</button>
+        {quizState.answerSelected && (
+          <button onClick={() => dispatch({type: "CHANGE_QUESTION"})}>Continuar</button>
+        )}
     </div>
 
   )
